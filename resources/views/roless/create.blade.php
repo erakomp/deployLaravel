151dabf6669deb9022c7dea874d2b6edea6684bd
Debita Josephine <debbyjosie21@gmail.com>
2021-11-11 09:03:41 +0700

    updated the frontend for the register roles

--- resources/views/roless/create.blade.php
+++ resources/views/roless/create.blade.php
@@ -3,14 +3,7 @@
 @section('content')
 <div class="container">
 <div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Add New Roles</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('roless.index') }}"> Back</a>
-        </div>
-    </div>
+    
 </div>
    
 @if ($errors->any())
@@ -23,43 +16,59 @@
         </ul>
     </div>
 @endif
-
-    <form action="{{ route('roless.store') }}" method="POST">
-        @csrf
-      
-         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Name:</strong>
-                    <select name="user_id" id="user_id">
-                        @foreach ($users as $item)
-                        <option value="{{$item->id}}">{{$item->name}}</option>
-
-                        @endforeach
-                        
-                      </select>
-                </div>
+<div class="card" style="background-color:white!important; padding:5%;">
+    <div class="container">
+        
+        <div class="card-body">
+            <div class="text-left" style="margin-bottom:3%;">
+                <h2>Register Roles</h2>
             </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Name:</strong>
-                    <select name="role_id" id="role_id">
-                        @foreach ($roles as $item)
-                        <option value="{{$item->id}}">{{$item->display_name}}</option>
-
-                        @endforeach
-                        
-                      </select>
-                </div>
-            </div>
-    
+                <form action="{{ route('roless.store') }}" method="POST">
+                    @csrf
+                  
+                     <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-12" style="margin-bottom:1%;">
+                            <div class="form-group">
+                                <strong>Name:</strong>
+                                <select name="user_id" id="user_id">
+                                    @foreach ($users as $item)
+                                    <option value="{{$item->id}}">{{$item->name}}</option>
             
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </div>
+                                    @endforeach
+                                    
+                                  </select>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12" style="margin-bottom:1%;">
+                            <div class="form-group">
+                                <strong>Name:</strong>
+                                <select name="role_id" id="role_id">
+                                    @foreach ($roles as $item)
+                                    <option value="{{$item->id}}">{{$item->display_name}}</option>
+            
+                                    @endforeach
+                                    
+                                  </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 margin-tb">
        
-    </form>
+                            <div class="pull-left" style="margin-bottom:1%;">
+                               
+                            </div>
+                        </div>
+                        
+                        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                            <a class="btn btn-primary" href="{{ route('roless.index') }}" style="margin-right:1%!important;"> Back</a>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </div>
+                   
+                </form>
+        </div>
+    </div>
+</div>
+
    </div>
 
 @endsection
