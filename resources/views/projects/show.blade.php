4d67814bb931e2d47c0b86ae024b167aa39b1268
Debita Josephine <debbyjosie21@gmail.com>
2021-11-15 10:13:50 +0700

    resized

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -29,7 +29,7 @@
                                   <div class="project-board-card">
                                     <div class="position-relative">
                                     </div>
-                                    <p class="project-board-card-title" style="font-size:14px!important; font-weight:bold;" ><a href="{{route('tasks.show', $task->external_id)}}" class="link-color">{{$task->title}}</a></p>
+                                    <p class="project-board-card-title" style="font-size:14px!important; font-weight:bold; width:300px!important; max-width:300px!important;" ><a href="{{route('tasks.show', $task->external_id)}}" class="link-color">{{$task->title}}</a></p>
                                     <div class="project-board-card-description" style="font-size:14px!important; width:300px!important; max-width:300px!important;">{!!$task->description!!}
                                         @if($task->task_status == 'ur')
                                         <span class="label label-warning">Under Review</span>
