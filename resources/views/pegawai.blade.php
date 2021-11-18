9036a22f557818a51b62bd4069a27465ff27b087
Debita Josephine <debbyjosie21@gmail.com>
2021-11-18 13:59:25 +0700

    updated icons for add label

--- resources/views/pegawai.blade.php
+++ resources/views/pegawai.blade.php
@@ -5,12 +5,12 @@
             <div class="card mt-5" style="background-color:white; padding:5%; margin-left:-10%;">
                 <div class="container">
                     <div class="card-header text-center">
-                        <h1>Labels
-    
+                        <h1 ><strong>LABELS AVAILABLE 
+                        </strong><a href="/pegawai/tambah" class="btn btn-primary" style="margin-left: 2%;"><i class="fas fa-plus" ></i></a>
                         </h1>
                     </div>
                     <div class="card-body" >
-                        <a href="/pegawai/tambah" class="btn btn-primary">Add</a>
+                        
                         <br/>
                         <br/>
                         <table class="table table-responsive  table-striped" style="max-width: 100%;">
@@ -28,7 +28,7 @@
                                     <td><button class="btn" style="background-color: {{ $p->alamat }}; width:100px; pointer-events: none!important; "></button></td>
                                     <td>
                                        
-                                        <a href="/pegawai/hapus/{{ $p->id }}" class="btn btn-danger">Hapus</a>
+                                        <a href="/pegawai/hapus/{{ $p->id }}" class="btn btn-danger"><i class="fas fa-trash-alt"></i></a>
                                     </td>
                                 </tr>
                                 @endforeach
