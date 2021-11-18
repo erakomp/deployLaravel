40682f839e5f0b7d93f044a47e047aaa8d9118a9
Debita Josephine <debbyjosie21@gmail.com>
2021-11-18 13:32:00 +0700

    updated the frontend of user list

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,55 +1,60 @@
 @extends('layouts.master')
  
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="text-center">
-            <h1 style="font-size:28px;"><strong>Users Lists</strong></h1>
-        </div>
-        <div class="col-lg-12 margin-tb">
-            
-            <div class="pull-right">
-                <a class="btn btn-success" href="{{ route('productss.create') }}" style="margin-bottom:15%;"> Create New Product</a>
+<div class="card shadow" style="background-color: white;">
+    <div class="card-body">
+        <div class="container">
+            <div class="row">
+                <div class="text-center">
+                    <h1 style="font-size:28px;"><strong>USERS LIST</strong></h1>
+                </div>
+                <div class="col-lg-12 margin-tb">
+                    
+                    <div class="pull-right">
+                        <a class="btn btn-success" href="{{ route('productss.create') }}" style="margin-bottom:15%;"> <i class="fas fa-user-plus"></i></a>
+                    </div>
+                </div>
             </div>
+           
+            @if ($message = Session::get('success'))
+                <div class="alert alert-success">
+                    <p>{{ $message }}</p>
+                </div>
+            @endif
+           
+            <table class="table table-bordered">
+                <tr>
+                    <th>No</th>
+                    <th>Name</th>
+                    <th>Details</th>
+                   
+                </tr>
+                @foreach ($products as $product)
+                <tr>
+                    <td>{{ ++$i }}</td>
+                    <td>{{ $product->name }}</td>
+                    <td>{{ $product->email }}</td>
+                    <!--<td>
+                        <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+           
+                            <a class="btn btn-info" href="{{ route('productss.show',$product->id) }}">Show</a>
+            
+                            <a class="btn btn-primary" href="{{ route('productss.edit',$product->id) }}">Edit</a>
+           
+                            @csrf
+                            @method('DELETE')
+              
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>-->
+                </tr>
+                @endforeach
+            </table>
+          
         </div>
     </div>
-   
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
-   
-    <table class="table table-bordered">
-        <tr>
-            <th>No</th>
-            <th>Name</th>
-            <th>Details</th>
-           
-        </tr>
-        @foreach ($products as $product)
-        <tr>
-            <td>{{ ++$i }}</td>
-            <td>{{ $product->name }}</td>
-            <td>{{ $product->email }}</td>
-            <!--<td>
-                <form action="{{ route('products.destroy',$product->id) }}" method="POST">
-   
-                    <a class="btn btn-info" href="{{ route('productss.show',$product->id) }}">Show</a>
-    
-                    <a class="btn btn-primary" href="{{ route('productss.edit',$product->id) }}">Edit</a>
-   
-                    @csrf
-                    @method('DELETE')
-      
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
-            </td>-->
-        </tr>
-        @endforeach
-    </table>
-  
 </div>
+
     
       
 @endsection
