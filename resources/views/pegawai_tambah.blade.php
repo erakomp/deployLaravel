abd7c01ab1897544bd510965664602d50ff5b672
Debita Josephine <debbyjosie21@gmail.com>
2021-11-22 13:24:23 +0700

    updated the frontend of create labels page

--- resources/views/pegawai_tambah.blade.php
+++ resources/views/pegawai_tambah.blade.php
@@ -1,51 +1,55 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
-        <title></title>
-    </head>
-    <body>
+@extends('layouts.master')
+@section('content')
+<div class="card shadow" style="background-color:white;">
+    <div class="card-body" style="padding:2%;">
         <div class="container">
             <div class="card mt-5">
                 <div class="card-header text-center">
-                    <h1>Add Labels</h1>
+                    <h1><strong>ADD LABEL</strong></h1>
                 </div>
                 <div class="card-body">
-                    <a href="/pegawai" class="btn btn-primary">Kembali</a>
+                    
                     <br/>
                     <br/>
                     
                     <form method="post" action="/pegawai/store">
  
                         {{ csrf_field() }}
- 
-                        <div class="form-group">
-                            <label>Nama</label>
-                            <input type="text" name="nama" class="form-control" placeholder="Title">
- 
-                            @if($errors->has('nama'))
-                                <div class="text-danger">
-                                    {{ $errors->first('nama')}}
-                                </div>
-                            @endif
- 
-                        </div>
- 
-                        <div class="form-group">
-                            <label>Alamat</label>
-                            <input name="alamat" type="color" class="form-control" >
- 
-                             @if($errors->has('alamat'))
-                                <div class="text-danger">
-                                    {{ $errors->first('alamat')}}
-                                </div>
-                            @endif
- 
-                        </div>
- 
-                        <div class="form-group">
-                            <input type="submit" class="btn btn-success" value="Simpan">
+ <div class="row">
+    <div class="col-sm-6">
+
+        <div class="form-group">
+            <label style="font-size:18px;"><strong>Tag Title</strong></label>
+            <input type="text" name="nama" class="form-control" placeholder="Title">
+
+            @if($errors->has('nama'))
+                <div class="text-danger">
+                    {{ $errors->first('nama')}}
+                </div>
+            @endif
+
+        </div>
+
+    </div>
+    <div class="col-sm-6">
+        <div class="form-group">
+            <label style="font-size:18px;"><strong>Color</strong></label>
+            <input name="alamat" type="color" class="form-control" >
+
+             @if($errors->has('alamat'))
+                <div class="text-danger">
+                    {{ $errors->first('alamat')}}
+                </div>
+            @endif
+
+        </div>
+
+    </div>
+ </div>
+                        
+                        <div class="form-group" style="display:flex; justify-content:center; margin-top:2%;">
+                            <a href="/pegawai" class="btn btn-primary" style="margin-right:5px; font-size:16px;">Kembali</a>
+                            <input type="submit" class="btn btn-success" style="font-size:16px;" value="Simpan">
                         </div>
  
                     </form>
@@ -53,5 +57,7 @@
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+</div>
+        
+@endsection
