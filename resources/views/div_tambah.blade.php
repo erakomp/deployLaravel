3b683cb738c1aa20d06a314d12233d4a590c737b
Debita Josephine <debbyjosie21@gmail.com>
2021-11-23 11:47:58 +0700

    button

--- resources/views/div_tambah.blade.php
+++ resources/views/div_tambah.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.master')
 @section('content')
         <div class="container">
-            <div class="card mt-5" style="background: white;">
+            <div class="card mt-5" style="background: white; ">
                <div class="container">
-                <div class="card-body" style="padding:2%;">
+                <div class="card-body" style="padding:5%;">
                     
                     <br/>
                     <br/>
@@ -11,37 +11,44 @@
                     <form method="post" action="/div/store">
  
                         {{ csrf_field() }}
- 
-                        <div class="form-group">
-                            <label>Division</label>
-                            <input type="text" name="division" class="form-control" placeholder="Division">
-                            
- 
-                            @if($errors->has('user_id'))
-                                <div class="text-danger">
-                                    {{ $errors->first('user_id')}}
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Division Title</label>
+                                    <input type="text" name="division" class="form-control" placeholder="Division Title">
+                                    
+         
+                                    @if($errors->has('user_id'))
+                                        <div class="text-danger">
+                                            {{ $errors->first('user_id')}}
+                                        </div>
+                                    @endif
+         
                                 </div>
-                            @endif
- 
-                        </div>
- 
-                        <div class="form-group">
-                            <label>Description</label>
-                           
-                            <input type="text" name="description" class="form-control" placeholder="Description">
-                            
- 
-                             @if($errors->has('role_id'))
-                                <div class="text-danger">
-                                    {{ $errors->first('role_id')}}
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Description</label>
+                                   
+                                    <input type="text" name="description" class="form-control" placeholder="Description">
+                                    
+         
+                                     @if($errors->has('role_id'))
+                                        <div class="text-danger">
+                                            {{ $errors->first('role_id')}}
+                                        </div>
+                                    @endif
+         
                                 </div>
-                            @endif
- 
+                            </div>
                         </div>
+                        
  
-                        <div class="form-group">
-                            <a href="/div" class="btn btn-primary">Back</a>
-                            <input type="submit" class="btn btn-success" value="Submit">
+                       
+ 
+                        <div class="form-group" style="display:flex; justify-content:center;">
+                            <a href="/div" class="btn btn-md btn-brand movedown" style="font-size:16px; margin-right:5px;">Back</a>
+                            <input type="submit" class="btn btn-md btn-brand movedown" style="font-size:16px; margin-right:5px;" value="Submit">
                         </div>
  
                     </form>
