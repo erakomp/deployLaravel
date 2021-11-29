bed11ecf56e98f6b563226e92a1a6b18b674ac71
Debita Josephine <debbyjosie21@gmail.com>
2021-11-29 11:33:52 +0700

    idk what the heck is going on

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,13 +15,6 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->accessKey = $this->getConfig('LTAI4GEBBJYKtgwbQwAzqph1');
-        $this->accessKeySecret = $this->getConfig('ZqYTl7TgHVzwMwaSOzQcCvMjBZKD17');
-        $this->inner = $this->getConfig('erakomp');
-//      选择默认bucket
-        if ($defult = $this->getConfig('default')) {
-            $this->bucket($defult);
-        }
         $this->middleware('auth');
     }
 
@@ -42,27 +35,6 @@ class HomeController extends Controller
     {
         \LogActivity::addToLog('My Testing Add To Log.');
         dd('log insert successfully.');
-    }
-    public function upload($key, $file, $options = array())
-    {
-        $info = array(
-            'Bucket' => $this->bucket,
-            'Key' => $key,
-        );
-
-        if ($file instanceof UploadedFile) {
-            $info['Content'] = fopen($file->getRealPath(), 'r');
-            $info['ContentLength'] = $file->getSize();
-            $info['ContentType'] = $file->getMimeType();
-        } else {
-            if (!is_string($file)) {
-                throw new LogicException('The second argument must be a String(File path) or instance of \Symfony\Component\HttpFoundation\File\UploadedFile');
-            }
-            $info['Content'] = fopen(realpath($file), 'r');
-            $info['ContentLength'] = filesize($file);
-        }
-
-        return $this->client->putObject(array_merge($info, $options));
     }
 
 
