01cc41f78b537869c024c17d3fa0a3e1cb5624a8
Debita Josephine <debbyjosie21@gmail.com>
2021-12-08 10:47:57 +0700

    image

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -187,7 +187,7 @@ class TasksController extends Controller
         //     }
         // }
         //Hack to make dropzone js work, as it only called with AJAX and not form submit
-        return response()->json(['task_external_id' => $task->external_id, 'project_external_id' => $project ? $project->external_id : null]);
+       
         return redirect()->route("tasks.show", $insertedExternalId);
     }
 
