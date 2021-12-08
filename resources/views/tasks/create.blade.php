b2409488a38745fcd4bde86aeae1840b707dc59b
Debita Josephine <debbyjosie21@gmail.com>
2021-12-08 13:31:26 +0700

    image

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -211,7 +211,7 @@
                 [ 'color', [ 'color' ] ],
                 [ 'para', [ 'ol', 'ul', 'paragraph'] ],
                 [ 'table', [ 'table' ] ],
-                [ 'insert', [ 'link', 'picture'] ],
+                [ 'insert', [ 'link'] ],
                 [ 'view', [ 'fullscreen' ] ]
             ],
              height:300,
