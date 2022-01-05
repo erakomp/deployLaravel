ae62f80b124e3c79ea84891a8fd0df25297213e0
Debita Josephine <debbyjosie21@gmail.com>
2022-01-05 11:01:15 +0700

    front freakin end

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -74,7 +74,7 @@
                 @foreach($statuses as $status)
                 <div class="project-board-list" style="background-color:{{$status->color}};">
                     <header style="background-color:{{$status->color}}!important;">{{ __($status->title)}}</header>
-                    <ul class="sortable" id="{{$status->title}}" data-status-external-id="{{$status->external_id}}" style="max-height: 25em;"  >
+                    <ul class="sortable" id="{{$status->title}}" data-status-external-id="{{$status->external_id}}" style="max-height: 25em; min-height:30em;"  >
                         
                             
                     
