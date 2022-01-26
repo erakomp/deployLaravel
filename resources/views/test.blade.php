ff963c46ace4a0921d891ed197aa0034fe057c40
Debita Josephine <debbyjosie21@gmail.com>
2022-01-26 14:36:00 +0700

    test

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -56,7 +56,7 @@
                         <input type="datetime-local" name="to" id="input" style="margin-right:2%;" value="" my-date-format="DD/MM/YYYY, hh:mm:ss" >
                         <input type="submit"  class="btn btn-md btn-brand movedown" value="Filter" style="font-size: 16px; ">
                         <a href="/test">
-                            <input  class="btn btn-md btn-brand movedown" value="Reset" style="font-size: 16px;">Reset</a>
+                            <input  class="btn btn-md btn-brand movedown" style="font-size: 16px;">Reset</a>
                         </form>
                         
                         <div class="row" style="text-align: center;">
